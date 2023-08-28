39e5c6938b1f9d7ae8084452a1cc25b8ff429044
Monika Rai <rai20monika@gmail.com>
2023-08-29 04:14:48 +0530

    update sql port

--- app/config.php
+++ app/config.php
@@ -1,6 +1,6 @@
 <?php
 
-$conn = mysqli_connect('localhost', 'root', '', 'property_app_db');
+$conn = mysqli_connect('localhost', 'root', '', 'property_app_db', '3307');
 if (!$conn) {
     echo 'connection error: ' . mysqli_connect_error();
 }
