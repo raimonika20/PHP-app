b85c2af51bee268e0b4fb520b47aa67bed7d6e99
Monika Rai <rai20monika@gmail.com>
2023-08-29 02:01:11 +0530

    move to app folder so lamp can run all

--- queries.sql
+++ queries.sql
@@ -1,3 +1,5 @@
+drop database if exists property_app_db;
+
 -- CREATE DATABASE PROPERTY_APP_DB;
 create database if not exists property_app_db;
 
@@ -5,7 +7,7 @@ use property_app_db;
 
 -- Path: queries.sql
 CREATE TABLE
-  users (
+  if not exists users (
     id SERIAL PRIMARY KEY,
     name VARCHAR(255) NOT NULL,
     email VARCHAR(255) NOT NULL,
@@ -14,13 +16,12 @@ CREATE TABLE
   );
 
 create table
-  properties (
+  if not exists properties (
     id serial primary key,
     status varchar(255) not null, -- sold/not sold
     price float not null,
     address varchar(255) not null,
     type varchar(255) not null, -- residential/commercial/industrial
     created_on timestamp not null default current_timestamp,
-    image_url varchar(255) not null,
-    FOREIGN KEY (owner) REFERENCES users (id) ON DELETE CASCADE
+    image_url varchar(255) not null
   );
