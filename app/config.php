93093553511c5c9c97077c164dee104fd91b8b2c
Monika Rai <rai20monika@gmail.com>
2023-08-29 03:10:34 +0530

    clean up

--- app/config.php
+++ app/config.php
@@ -3,6 +3,4 @@
 $conn = mysqli_connect('localhost', 'admin', '5AzmLmnls9Eh', 'property_app_db');
 if (!$conn) {
     echo 'connection error: ' . mysqli_connect_error();
-} else {
-    echo 'connected successfully!';
 }
