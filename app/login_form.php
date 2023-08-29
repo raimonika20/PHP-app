5553eed7ec026ace635ca98425a69b70273eb5c9
Monika Rai <rai20monika@gmail.com>
2023-08-30 04:11:03 +0530

    login form updated

--- app/login_form.php
+++ app/login_form.php
@@ -8,7 +8,7 @@ if (isset($_POST['submit'])) {
     $select = "SELECT * FROM users WHERE email = '$email' && password = '$pass' ";
     $result = mysqli_query($conn, $select);
     if (mysqli_num_rows($result) > 0) {
-        header('property_page.php?type=Residential');
+        header('location:property_page.php?type=Residential');
     } else {
         header('location:login_form.php?error=login');
     }
@@ -38,7 +38,8 @@ if (isset($_POST['submit'])) {
                 <h3>Login</h3>
                 <input type="email" name="email" required placeholder="enter your email"><br>
                 <input type="password" name="password" required placeholder="enter your password"><br>
-                <input type="submit" name="submit" value="Login" class="form-btn">
+                <input type="submit" href="property_page.php?type=Residential" name="submit" value="Login"
+                    class="form-btn">
                 <?php
                 $error = $_GET['error'];
                 if (isset($error)) {
